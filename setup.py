0c8933420abfd841fdc5a0b4f0a7111e611f78a2
nathom <nathanthomas707@gmail.com>
2021-03-25 10:33:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-pkg_name = "music-dl"
+pkg_name = "streamrip"
 
 
 def read_file(fname):
@@ -14,27 +14,24 @@ requirements = read_file("requirements.txt").strip().split()
 # https://github.com/pypa/sampleproject/blob/main/setup.py
 setup(
     name=pkg_name,
-    version="0.9.7",
-    url="https://github.com/vitiko98/Qobuz-DL",
+    version="0.1",
     install_requires=requirements,
+    py_modules=["streamrip"],
     entry_points={
         "console_scripts": [
-            "music-dl = music_dl:main",
-            "rip = music_dl:main",
+            "rip = music_dl.cli:main",
+            "streamrip = music_dl.cli:main",
         ],
     },
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
     ],
-    package_dir={"", "music-dl"},
-    packages=find_packages(where="music-dl"),
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     project_urls={
         "Bug Reports": "https://github.com/nathom/music-dl/issues",
         "Source": "https://github.com/nathom/music-dl",
