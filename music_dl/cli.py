5c34dea6c2da6610b73ddc8227a8b484d8ef2fd7
nathom <nathanthomas707@gmail.com>
2021-03-25 10:32:46 -0700

    Split interactive subcommand into search and discover

--- music_dl/cli.py
+++ music_dl/cli.py
@@ -4,7 +4,7 @@ import os
 import click
 
 from .config import Config
-from .constants import CACHE_DIR, CONFIG_DIR, CONFIG_PATH
+from .constants import CACHE_DIR, CONFIG_DIR, CONFIG_PATH, QOBUZ_FEATURED_KEYS
 from .core import MusicDL
 from .utils import init_log
 
@@ -90,7 +90,7 @@ def cli(ctx, **kwargs):
 @click.argument("URLS", nargs=-1)
 @click.pass_context
 def filter_discography(ctx, **kwargs):
-    """ONLY AVAILABLE FOR QOBUZ
+    """Filter an artists discography (qobuz only).
 
     The Qobuz API returns a massive number of tangentially related
     albums when requesting an artist's discography. This command
@@ -108,18 +108,48 @@ def filter_discography(ctx, **kwargs):
 
 
 @cli.command()
-@click.option("-t", "--type", default="album")
-@click.option("-s", "--source", default="qobuz")
-@click.option("-d", "--discover", is_flag=True)
+@click.option("-t", "--type", default="album", help="album, playlist, track, or artist")
+@click.option("-s", "--source", default="qobuz", help="qobuz, tidal, or deezer")
 @click.argument("QUERY", nargs=-1)
 @click.pass_context
-def interactive(ctx, **kwargs):
-    """Interactive search for a query. This will display a menu
-    from which you can choose an item to download.
+def search(ctx, **kwargs):
+    """Search and download media in interactive mode.
 
-    If the source is Qobuz, you can use the `--discover` option with
-    one of the following queries to fetch and interactively download
-    the featured albums.
+    Example usage:
+
+        $ rip search 'fleetwood mac rumours'
+
+        Search for a Qobuz album that matches 'fleetwood mac rumours'
+
+        $ rip search -t track 'back in the ussr'
+
+        Search for a Qobuz track with the given query
+
+        $ rip search -s tidal 'jay z 444'
+
+        Search for a Tidal album that matches 'jay z 444'
+
+    """
+    if isinstance(kwargs["query"], (list, tuple)):
+        query = " ".join(kwargs["query"])
+    elif isinstance(kwargs["query"], str):
+        query = kwargs["query"]
+    else:
+        raise ValueError("Invalid query type" + type(kwargs["query"]))
+
+    if core.interactive_search(query, kwargs["source"], kwargs["type"]):
+        core.download()
+    else:
+        click.secho("No items chosen, exiting.", fg="bright_red")
+
+
+@cli.command()
+@click.option("-l", "--list", default="ideal-discography")
+@click.pass_context
+def discover(ctx, **kwargs):
+    """Searches for albums in Qobuz's featured lists.
+
+    Avaiable options for `--list`:
 
         * most-streamed
 
@@ -150,21 +180,19 @@ def interactive(ctx, **kwargs):
         * universal-jeunesse
 
         * universal-chanson
-
     """
+    assert (
+        kwargs["list"] in QOBUZ_FEATURED_KEYS
+    ), f"Invalid featured key {kwargs['list']}"
 
-    logger.debug(f"starting interactive mode for type {kwargs['type']}")
-    if kwargs["discover"]:
-        if kwargs["query"] == ():
-            kwargs["query"] = ["ideal-discography"]
-        kwargs["type"] = "featured"
-
-    query = " ".join(kwargs["query"])
-
-    if core.interactive_search(query, kwargs["source"], kwargs["type"]):
+    if core.interactive_search(kwargs["list"], "qobuz", "featured"):
         core.download()
     else:
-        click.secho("No items chosen, exiting.", fg="bright_red")
+        none_chosen()
+
+
+def none_chosen():
+    click.secho("No items chosen, exiting.", fg="bright_red")
 
 
 def parse_urls(arg: str):
@@ -177,10 +205,4 @@ def parse_urls(arg: str):
 
 
 def main():
-    cli.add_command(filter_discography)
-    cli.add_command(interactive)
     cli(obj={})
-
-
-if __name__ == "__main__":
-    main()
