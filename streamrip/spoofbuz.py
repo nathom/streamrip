e86e560ce0d5895defd64e9aee8b21a18201aa81
Nathan Thomas <nathanthomas707@gmail.com>
2022-05-23 18:02:57 -0700

    Fix Qobuz app_id error #333

--- streamrip/spoofbuz.py
+++ streamrip/spoofbuz.py
@@ -28,7 +28,7 @@ class Spoofer:
         self.app_id_regex = (
             r'{app_id:"(?P<app_id>\d{9})",app_secret:"\w{32}",base_port:"80"'
             r',base_url:"https://www\.qobuz\.com",base_method:"/api\.js'
-            r'on/0\.2/"},n\.base_url="https://play\.qobuz\.com"'
+            r'on/0\.2/"},n\.base_url="https://nightly-play\.qobuz\.com"'
         )
         login_page_request = requests.get("https://play.qobuz.com/login")
         login_page = login_page_request.text
