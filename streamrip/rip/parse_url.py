e835902471d1f41e8b1fa5f09c74c8956d9ba13a
Nathan Thomas <nathanthomas707@gmail.com>
2023-12-22 13:26:51 -0800

    Remove validation regex file

--- streamrip/rip/parse_url.py
+++ streamrip/rip/parse_url.py
@@ -14,11 +14,17 @@ from ..media import (
     PendingPlaylist,
     PendingSingle,
 )
-from .validation_regexps import (
-    QOBUZ_INTERPRETER_URL_REGEX,
-    SOUNDCLOUD_URL_REGEX,
-    URL_REGEX,
+
+URL_REGEX = re.compile(
+    r"https?://(?:www|open|play|listen)?\.?(qobuz|tidal|deezer)\.com(?:(?:/(album|artist|track|playlist|video|label))|(?:\/[-\w]+?))+\/([-\w]+)",
 )
+SOUNDCLOUD_URL_REGEX = re.compile(r"https://soundcloud.com/[-\w:/]+")
+LASTFM_URL_REGEX = re.compile(r"https://www.last.fm/user/\w+/playlists/\w+")
+QOBUZ_INTERPRETER_URL_REGEX = re.compile(
+    r"https?://www\.qobuz\.com/\w\w-\w\w/interpreter/[-\w]+/[-\w]+",
+)
+DEEZER_DYNAMIC_LINK_REGEX = re.compile(r"https://deezer\.page\.link/\w+")
+YOUTUBE_URL_REGEX = re.compile(r"https://www\.youtube\.com/watch\?v=[-\w]+")
 
 
 class URL(ABC):
