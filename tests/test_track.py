bfac933203ac27932ff0a26b1e2121b85bc24628
draco <me@dracoftw.com>
2023-12-26 21:35:21 +0100

    add dummy database as missing arg

--- tests/test_track.py
+++ tests/test_track.py
@@ -6,11 +6,12 @@ from util import arun
 from streamrip.client.downloadable import Downloadable
 from streamrip.client.qobuz import QobuzClient
 from streamrip.media.track import PendingSingle, Track
+import streamrip.db as db
 
 
 def test_pending_resolve(qobuz_client: QobuzClient):
     qobuz_client.config.session.downloads.folder = "./tests"
-    p = PendingSingle("19512574", qobuz_client, qobuz_client.config)
+    p = PendingSingle("19512574", qobuz_client, qobuz_client.config, db.Database(db.Dummy(), db.Dummy()))
     t = arun(p.resolve())
     dir = "tests/Fleetwood Mac - Rumours (1977) [FLAC] [24B-96kHz]"
     assert os.path.isdir(dir)
