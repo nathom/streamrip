5a987a59fda42d7c76e2d1b6a24d776d63999432
Nathan Thomas <nathanthomas707@gmail.com>
2024-01-23 18:02:45 -0800

    Fix bug where max_cover_with doesnt work

--- streamrip/media/artwork.py
+++ streamrip/media/artwork.py
@@ -125,13 +125,14 @@ def downscale_image(input_image_path: str, max_dimension: int):
 
 
     """
+    logger.info("Downscaling image at {input_image_path} to ")
     # Open the image
     image = Image.open(input_image_path)
 
     # Get the original width and height
     width, height = image.size
 
-    if max_dimension <= max(width, height):
+    if max_dimension >= max(width, height):
         return
 
     # Calculate the new dimensions while maintaining the aspect ratio
