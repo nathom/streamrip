fe4d25ac09967b37d966bd2cfa4bc396d12c7b93
Rodrigo Machado <29443508+ralmachado@users.noreply.github.com>
2024-10-24 00:33:27 +0100

    Fixes unguarded remove from list (closes #752)

--- streamrip/client/qobuz.py
+++ streamrip/client/qobuz.py
@@ -117,7 +117,8 @@ class QobuzSpoofer:
             ).decode("utf-8")
 
         vals: List[str] = list(secrets.values())
-        vals.remove("")
+        while "" in vals:
+            vals.remove("")
 
         secrets_list = vals
 
